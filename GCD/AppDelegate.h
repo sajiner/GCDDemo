ca9fc3e2dc03e43a265c3e175f94f9ed192e5148
sajiner <739134362@qq.com>
2017-06-11 23:28:19 +0800

    Initial Commit

---
+++ GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by 张鑫 on 2017/6/11.
+//  Copyright © 2017年 张鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by 张鑫 on 2017/6/11.
+//  Copyright © 2017年 张鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
